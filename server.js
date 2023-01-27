48b07375b1be7c52814356adc7921fe7752154d4
leap2022 <sharawnyambuuurtnasan@gmail.com>
2023-01-27 08:47:04 +0800

    5dahiin ugluu

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const cors = require("cors");
 const app = express();
 const port = 2022;
 const data = require("./data.json");
+const { request, response } = require("express");
 app.use(cors());
 app.get("/products", (request, response) => {
   console.log("start");
-  response.status(200).json(data.product);
+  response.status(200).json(data.products);
 });
 
-app.get("/order", (request, response) => {
-  response.status(201).json(data.order);
+app.get("/orders", (request, response) => {
+  response.status(201).json(data.orders);
 });
+app.get("/moderators", (request, response)=>{
+  response.status(202).json(data.moderats)
+})
+
+app.get("/users", (request, response)=>{
+  response.status(203).json(data.users)
+})
 
 app.listen(port, () => {
   console.log(`server is starting in ${port}`);
