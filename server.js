540c996eaa2ae8c2fb8f840d7c28119811bbe7be
Morning <morning@admins-iMac-2.local>
2023-01-27 12:37:09 +0800

    post

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const port = 2022;
 const data = require("./data.json");
 const { request, response } = require("express");
 app.use(cors());
+app.use(express.json());
+
+app.post("/products", (request, response) => {
+  data.products.push(request.body);
+});
 app.get("/products", (request, response) => {
   console.log("start");
   response.status(200).json(data.products);
@@ -14,13 +19,13 @@ app.get("/products", (request, response) => {
 app.get("/orders", (request, response) => {
   response.status(201).json(data.orders);
 });
-app.get("/moderators", (request, response)=>{
-  response.status(202).json(data.moderats)
-})
+app.get("/moderators", (request, response) => {
+  response.status(202).json(data.moderats);
+});
 
-app.get("/users", (request, response)=>{
-  response.status(203).json(data.users)
-})
+app.get("/users", (request, response) => {
+  response.status(203).json(data.users);
+});
 
 app.listen(port, () => {
   console.log(`server is starting in ${port}`);
